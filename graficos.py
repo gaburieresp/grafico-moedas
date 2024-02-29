05be5734e45d0799f0c6fe1194ef8a303224e88c
Gabriele Pereira <120118227+gaburieresp@users.noreply.github.com>
2024-02-29 08:34:01 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -32,17 +32,3 @@ def menu():
     print('3 - Gráfico de dispersão')
     print('0 - Sair')
     print()
-
-opcao = 1
-
-while opcao != 0:
-    menu()
-    opcao = int(input('Escolha um tipo de gráfico: '))
-
-    match opcao:
-        case 1: grafico_barra(l_moedas, l_valores)
-        case 2: grafico_pizza(l_moedas, l_valores)
-        case 3: grafico_dispersao(l_moedas, l_valores)
-
-print()
-print('Até a próxima!')
